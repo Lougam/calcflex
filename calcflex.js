b9ee54291d7f4a44e535385586e174ebdcf2c6ab
andrelougam <108102295+andrelougam@users.noreply.github.com>
2022-08-01 20:59:30 -0300

    Add files via upload

---
+++ calcflex.js
@@ -0,0 +1,34 @@
+/*author Andre Lougam*/
+
+var etanol,gasolina;
+var comb = ''
+var msg = document.getElementById('msg')
+
+function calcular(){
+
+ etanol= parseFloat(frmFlex.txtEtanol.value.replace(",","."));
+
+ gasolina= parseFloat(frmFlex.txtGasolina.value.replace(",","."));
+ 
+
+ if(etanol < 0.74 * gasolina)
+ {
+/*verdadeiro*/
+comb ='ETANOl'
+document.getElementById("status").src="imagens/etanol.png"
+ }
+
+ else{
+/*falso*/ document.getElementById("status").src="imagens/gasolina.png"
+comb ='GASOLINA'
+ }
+
+msg.style.textAlign = 'center'
+msg.style.fontFamily = 'arial'
+msg.style.fontStyle = 'bold'
+msg.innerHTML= `É MAIS VANTAJOSO USAR: <strong><u>${comb}</u></strong>.`
+}
+function resetar() {
+    document.getElementById("status").src="imagens/neutro.png"
+}
+
